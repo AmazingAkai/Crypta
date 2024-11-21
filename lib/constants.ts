b9d0ec0efdf7105aea10914f09e6f19e3a583bbb
AmazingAkai <akai.is.amazing@gmail.com>
2024-11-21 21:13:09 +0530

    update descriptions.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,12 +1,10 @@
 export const models = [
   {
     name: "meta-llama/Meta-Llama-3.1-405B-Instruct",
-    description:
-      "Meta's premiere model. Friendly, smart, and a strong creative writer.",
+    description: "Meta's premiere model. Friendly and a smart creative writer.",
   },
   {
     name: "Qwen/Qwen2.5-72B-Instruct",
-    description:
-      "Alibaba's largest open model. It's close to Meta Llama 3 at English tasks.",
+    description: "Alibaba's largest open model. It's close to Meta Llama 3.",
   },
 ];
